78e44f5e2207653668429548217ba508e802f338
jmgiannetti <90001880+jmgiannetti@users.noreply.github.com>
2021-09-05 21:00:55 -0400

    Update HelloWorld.js

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -27,11 +27,22 @@ export class HelloWorld extends LitElement {
   __increment() {
     this.counter += 1;
   }
-
+  
+  //Adding decrement button in order to lower the counter.
+  
+__decrement() {
+    if(this.counter >= 0){
+      this.counter --;
+    }
+  }
+  
+  //Line 45- Additional code that disables the decrement button whenever the value is equal to 0
+  
   render() {
     return html`
       <h2>${this.title} Nr. ${this.counter}!</h2>
       <button @click=${this.__increment}>increment</button>
+      <button @click=${this.__decrement} ?disabled=${this.counter == 0}>decrement</button>
     `;
   }
 }
